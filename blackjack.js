c2b7e67e0521a6b33c43daca648709f936d3099f
CTP <ctp@CTPs-MBP.fios-router.home>
2021-01-05 21:22:39 -0500

    Add Bust, Hit, Stand

--- blackjack.js
+++ blackjack.js
@@ -1,14 +1,17 @@
 // The Game of Black is to have a hand that totals higher than the dealers.
-// In the main function, put 2 thru 10, "Ace", "Jack", "Queen", "King" inside an array called Deal. the user is given two numbers(cards) at random from the array, the computer is given two numbers but only shows one.
-// create a function called Bust that has an if card amount is greater than 21 "it's a Bust you lose" end Game else continue
-// create a function called Hit that gives you a random card to add in your deck 
+// In the main function, put 2 thru 10, "Ace", "Jack", "Queen", "King" inside an array called Deck. 
+// The user is given two numbers(cards) at random from the array, the computer is given two numbers but only shows one.
+// Create a function called Deck that lines up all the cards (Spades Hearts Clover Diamond)
+// create a function called Bust that takes a parameter has an if card amount is greater than 21 "it's a Bust you lose" end Game else continue
+// create a function called Hit that gives you a random card to add in your deck
 // create a function called Stand that returns your deck
 
 
 var Suits = ["Clover", "Hearts", "Spade", "Diamonds"];
 var Faces = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
 var deck = new Array();
-
+let count;
+let array = [];
 function Deck()
 {
   deck = new Array();
@@ -20,11 +23,30 @@ function Deck()
       deck.push(card)
     }
   }
-  return deck;
+
 }
- console.log(Deck());
+ //console.log(Deck());
 
+ 
+function Bust (count)
+{
+    if (count > 21)
+    {
+        console.log("it's a Bust you lose");
+        break;
+    }
+    return 0;
+}
 
+function Hit ()
+{
+    let randomCard = Math.floor(Math.random() * deck.length);
+    return randomCard;
+}
 
+function Stand(array)
+{
+    return array;
+}
 
     
